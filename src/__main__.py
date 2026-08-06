3c24f350510f32fd80acff5a55279898b771fbb6
llourens <llourens@f0r3s7.codam.nl>
2026-08-06 16:08:21 +0200

    🚧 Minor changes

--- src/__main__.py
+++ src/__main__.py
@@ -2,22 +2,27 @@ from src.input_checking import input_checking
 from json import JSONDecodeError
 from pydantic import ValidationError
 from colorama import Fore
+from src.encoding import stage_encoding
+from llm_sdk import Small_LLM_Model
 
 
 def call_me_maybe() -> None:
-
     print(Fore.LIGHTBLUE_EX + "Checking input... ->", end=" ")
     try:
-        functions_definition_list, input_prompts_list = input_checking()
+        json_contents = input_checking()
+        functions_definition_list, input_prompts_list = json_contents
         print(Fore.GREEN + "Input verified")
     except (FileExistsError, FileNotFoundError, JSONDecodeError,
             ValidationError, KeyError) as msg:
         print(Fore.RED + str(msg))
         return
+    print(Fore.LIGHTBLUE_EX + "Starting LLM... ->" + Fore.WHITE)
+    llm = Small_LLM_Model()
     for prompt_nbr, prompt in enumerate(input_prompts_list):
         print(Fore.LIGHTBLUE_EX +
               f"Processing prompt number {prompt_nbr + 1}...")
         print(Fore.GREEN + f"Prompt {prompt_nbr + 1}: Encoding...")
+        stage_encoding(json_contents, llm)
         print(f"Prompt {prompt_nbr + 1}: Being fed to the model...")
         print(f"Prompt {prompt_nbr + 1}: Processing logits...")
         print(f"Prompt {prompt_nbr + 1}: Decoding...")
---
+++ src/encoding.py
@@ -0,0 +1,7 @@
+from typing import Any
+from llm_sdk import Small_LLM_Model
+
+
+def stage_encoding(json_lists: tuple[list[Any], list[Any]],
+                   llm: Small_LLM_Model) -> list[int]:
+    return []
