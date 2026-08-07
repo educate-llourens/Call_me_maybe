96475b27ddde0dd531e0ed1ffe8649860c2be8eb
LionessLilo <leandra@lionesslilo.com>
2026-08-07 19:54:09 +0200

    🚧 More reasearch than progress

--- src/__main__.py
+++ src/__main__.py
@@ -2,11 +2,18 @@ from src.input_checking import input_checking
 from json import JSONDecodeError
 from pydantic import ValidationError
 from colorama import Fore
+from typing import Any
 from src.encoding import stage_encoding
 from llm_sdk import Small_LLM_Model
 
 
 def call_me_maybe() -> None:
+    json_contents = tuple[list[Any], list[Any]]
+    functions_definition_list: list[Any]
+    input_prompts_list: list[Any]
+    llm: Small_LLM_Model
+    encoded_tokens: list[int]
+
     print(Fore.LIGHTBLUE_EX + "Checking input... ->", end=" ")
     try:
         json_contents = input_checking()
@@ -22,7 +29,8 @@ def call_me_maybe() -> None:
         print(Fore.LIGHTBLUE_EX +
               f"Processing prompt number {prompt_nbr + 1}...")
         print(Fore.GREEN + f"Prompt {prompt_nbr + 1}: Encoding...")
-        stage_encoding(json_contents, llm)
+        encoded_tokens = stage_encoding(functions_definition_list,
+                                        input_prompts_list, llm)
         print(f"Prompt {prompt_nbr + 1}: Being fed to the model...")
         print(f"Prompt {prompt_nbr + 1}: Processing logits...")
         print(f"Prompt {prompt_nbr + 1}: Decoding...")
--- src/encoding.py
+++ src/encoding.py
@@ -2,6 +2,14 @@ from typing import Any
 from llm_sdk import Small_LLM_Model
 
 
-def stage_encoding(json_lists: tuple[list[Any], list[Any]],
+def stage_encoding(functions_definition_list: list[Any],
+                   input_prompts_list: list[Any],
                    llm: Small_LLM_Model) -> list[int]:
+    encode_string: str
+
+    encode_string = (
+        'You can only choose one function from this ordered list to solve the '
+        'prompt. Return the number of the correct function, and the parameters'
+        ' in this format: "parameters": {"a": 2.0, "b": 3.0}'
+    )
     return []
